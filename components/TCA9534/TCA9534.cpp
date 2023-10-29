acd73276d4a7f0ce3aec9d4bdf740682790883d8
Jdominguez991 <73549619+Jdominguez991@users.noreply.github.com>
2023-10-29 15:07:49 -0400

    fixing naming to 9534

--- components/TCA9534/TCA9534.cpp
+++ components/TCA9534/TCA9534.cpp
@@ -9,7 +9,7 @@ namespace tca9534a {
 
 static const char *const TAG = "tca9534a";
 
-void TCA9548AComponent::setup() {
+void TCA9534AComponent::setup() {
   ESP_LOGCONFIG(TAG, "Setting up TCA9534A...");
   Wire.begin();
   my_GPIO=new TCA9534();
@@ -21,7 +21,7 @@ void TCA9548AComponent::setup() {
   myGPIO.pinMode(currentPinMode);//Use GPIO_OUT and GPIO_IN instead of OUTPUT and INPUT_PULLUP
 }
 
-void TCA9548AComponent::dump_config() {
+void TCA9534AComponent::dump_config() {
   ESP_LOGCONFIG(TAG, "TCA9534A:");
   LOG_I2C_DEVICE(this);
 }
